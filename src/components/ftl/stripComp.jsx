bb5dfea049f736f6330e878a873fdea90a48bb92
Sahil Kalkal <sahil@flyde.in>
2025-08-27 19:39:18 +0530

    final 2

--- src/components/ftl/stripComp.jsx
+++ src/components/ftl/stripComp.jsx
@@ -1,21 +1,8 @@
-import { useEffect, useState } from "react";
 import { MoveRightIcon } from "lucide-react";
 
 export default function StripFtl({ text, button, m }) {
-  const [marginTop, setMarginTop] = useState("78px");
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (window.innerWidth >= 640) {
-        setMarginTop(`${m}px`);
-      } else {
-        setMarginTop("78px");
-      }
-    }
-  }, [m]);
-
   return (
-    <div className="counterSection1 sm:mt-0" style={{ marginTop }}>
+    <div className="counterSection1" style={{ marginTop: `${m}px` }}>
       <div className="global-container flex items-center gap-[26px]">
         <h2
           className="red-strip-text"

49896582c324fe2dc355a07b6943f18a219cd88c
Sahil Kalkal <sahil@flyde.in>
2025-09-05 15:30:54 +0530

    fixed spacing issue 2

--- src/components/ftl/TruckImageSection.jsx
+++ src/components/ftl/TruckImageSection.jsx
@@ -13,16 +13,20 @@ export default function TruckImageSection({
   const [calcValue, setCalcValue] = useState(0);
 
   useEffect(() => {
-    if (truckRef.current && overlayRef.current) {
-      const truckHeight = truckRef.current.offsetHeight;
-      const overlayHeight = overlayRef.current.offsetHeight;
+    const calculateValue = () => {
+      if (truckRef.current && overlayRef.current) {
+        const truckHeight = truckRef.current.offsetHeight;
+        const overlayHeight = overlayRef.current.offsetHeight;
 
-      // Formula: (overlay height + 78 - truck height) / 2
-      const value = overlayHeight + 78 - truckHeight / 2;
-      console.log("Calculated Value:", value);
-      setCalcValue(value);
-      setM(value);
-    }
+        // Formula: (overlay height + 78 - truck height) / 2
+        const value = overlayHeight + 78 - truckHeight / 2;
+        console.log("Calculated Value:", value);
+        setCalcValue(value);
+        setM(value);
+      }
+    };
+
+    calculateValue();
 
     const observer = new ResizeObserver(calculateValue);
 
@@ -34,8 +38,6 @@ export default function TruckImageSection({
       if (overlayRef.current) observer.unobserve(overlayRef.current);
     };
   }, [setM]);
-
-
 
   return (
     <div className="global-container lg:mt-[78px] mt-[42px]">

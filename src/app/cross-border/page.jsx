7efc0f0ed541fb3ac1ccd1e1998fe29cb9a77d89
Anchal <Anchal@flyde.in>
2025-09-05 17:07:09 +0530

    live 3

--- src/app/cross-border/page.jsx
+++ src/app/cross-border/page.jsx
@@ -23,37 +23,37 @@ export default function TempControlled() {
   }, [skip]);
 
   // Wait for all images to load before hiding loader
-  // useEffect(() => {
-  //   const images = Array.from(document.querySelectorAll("img"));
-  //   let loadedCount = 0;
+  useEffect(() => {
+    const images = Array.from(document.querySelectorAll("img"));
+    let loadedCount = 0;
 
-  //   if (images.length === 0) {
-  //     PageContentReady();
-  //     return;
-  //   }
+    if (images.length === 0) {
+      PageContentReady();
+      return;
+    }
 
-  //   const handleLoad = () => {
-  //     loadedCount++;
-  //     if (loadedCount === images.length) {
-  //       PageContentReady();
-  //     }
-  //   };
+    const handleLoad = () => {
+      loadedCount++;
+      if (loadedCount === images.length) {
+        PageContentReady();
+      }
+    };
 
-  //   images.forEach((img) => {
-  //     if (img.complete) handleLoad();
-  //     else {
-  //       img.addEventListener("load", handleLoad);
-  //       img.addEventListener("error", handleLoad);
-  //     }
-  //   });
+    images.forEach((img) => {
+      if (img.complete) handleLoad();
+      else {
+        img.addEventListener("load", handleLoad);
+        img.addEventListener("error", handleLoad);
+      }
+    });
 
-  //   return () => {
-  //     images.forEach((img) => {
-  //       img.removeEventListener("load", handleLoad);
-  //       img.removeEventListener("error", handleLoad);
-  //     });
-  //   };
-  // }, [PageContentReady]);
+    return () => {
+      images.forEach((img) => {
+        img.removeEventListener("load", handleLoad);
+        img.removeEventListener("error", handleLoad);
+      });
+    };
+  }, [PageContentReady]);
 
   // Scroll detection for sticky header
   useEffect(() => {

2137ff02071fe3f9bfbb576722ae93ae2dae71d2
Anchal <Anchal@flyde.in>
2025-09-09 16:38:42 +0530

    live 7

--- src/components/careers/HeroSection.jsx
+++ src/components/careers/HeroSection.jsx
@@ -5,7 +5,7 @@ export default function HeroSection({ isScrolled }) {
   return (
     <>
       <HeroSectionComponent
-        imageSrc="/images/sustainability hero image.png"
+        imageSrc="https://ik.imagekit.io/a9uxeuyhx/Image2.png?updatedAt=1757408919424"
         title={`Join Our Growing Team`}
         subtitle=" Why Choose Transexpert?        "
         description="We're committed to providing our team members with the support,

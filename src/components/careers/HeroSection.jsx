40f666f6e69ba2faf178d6c156ae97a1ab68e4d3
Anchal <Anchal@flyde.in>
2025-09-10 17:00:48 +0530

    fix jira issue 12

--- src/components/careers/HeroSection.jsx
+++ src/components/careers/HeroSection.jsx
@@ -5,7 +5,7 @@ export default function HeroSection({ isScrolled }) {
   return (
     <>
       <HeroSectionComponent
-        imageSrc="/images/sustainability hero image.png"
+        imageSrc="https://ik.imagekit.io/a9uxeuyhx/sustainability%20hero%20image.png?updatedAt=1757503820575"
         title={`Join Our Growing Team`}
         subtitle=" Why Choose Transexpert?        "
         description="We're committed to providing our team members with the support,

50be51f46184cfe906a0547ed581860a3842778f
Anchal <Anchal@flyde.in>
2025-09-15 18:27:48 +0530

    push 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,11 @@ import GoogleTranslator from "@/components/googleTranslator";
 import { Onest } from "next/font/google";
 
 const onest = Onest({ subsets: ["latin"], display: "swap" });
-
 export const metadata = {
   title: "TransExpert – North American Logistics & Cross-Border Freight",
   description:
     "Reliable cross-border logistics from Canada to the U.S. with temperature-controlled, expedited, and same-day delivery.",
   metadataBase: new URL("https://transexpert.ca"),
-
-  
   openGraph: {
     title: "TransExpert – Logistics Across North America",
     description:
@@ -20,8 +17,7 @@ export const metadata = {
     siteName: "TransExpert",
     images: [
       {
-        url: "/images/previewLink.png",
-
+        url: "src/app/opengraph-image.png.png",
         width: 1200,
         height: 630,
         alt: "TransExpert Freight & Logistics",
@@ -30,16 +26,15 @@ export const metadata = {
     locale: "en_CA",
     type: "website",
   },
-
-  // ✅ Twitter Card
   twitter: {
     card: "summary_large_image",
     title: "TransExpert – Trusted North American Logistics",
     description:
       "Your source for efficient cross-border freight services between Canada and the U.S.",
-    images: ["/images/previewLink.png"], // same OG image
+    images: ["src/app/opengraph-image.png.png"],
   },
 };
+
 
 export default function RootLayout({ children }) {
   return (
